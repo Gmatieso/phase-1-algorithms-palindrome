4abbe7da0cfb41720b5fbf717281b13209e0e421
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-29 15:38:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  let j = word.length -1;
+
+  for( let i = 0; i < j/2; i++){
+    // forward character
+    let x = word[i];
+
+    // backward character
+    let y = word[j-1];
+
+    if(x != y ){
+
+      // return false if string not match 
+      return false;
+    }
+  }
+      // return true if string is palindrome 
+      return true;
 }
+
+let word = "racecar";
+isPalindrome(word)
 
 /* 
   Add your pseudocode here
+
+  Approach1:
+  First iterate over a string in forward and backward direction.
+
+  Check if all foward and backward character matches, return true.
+
+  if all forward and backward character does not match, return false
+  else return is true, it is a palindrome.
 */
+
+
 
 /*
   Add written explanation of your solution here
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
