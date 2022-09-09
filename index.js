8d162e4f6e589f1814eea21afa25e590e4ad089d
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-09-09 15:32:30 +0300

    :poop:  Writing bad code that needs to be improved

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-function isPalindrome(word) {
-  
-  let j = word.length -1;
+function reverse(word){
+// 'abc' => 'cba'
+const wordArray = word.split("")
+const reversedWordArray =  wordArray.reverse()
+const reversedWord = reversedWordArray.join("")
 
-  for( let i = 0; i < j/2; i++){
-    // forward character
-    let x = word[i];
-
-    // backward character
-    let y = word[j-1];
-
-    if(x != y ){
-
-      // return false if string not match 
-      return false;
-    }
-  }
-      // return true if string is palindrome 
-      return true;
+return reversedWord
 }
 
-let word = "racecar";
-isPalindrome(word)
+function isPalindrome(word) {
+
+  // return the input string 
+ const reversedWord = reverse(word)
+
+ if (word === reversedWord){
+
+  return true
+
+ }else{
+
+  return false
+ }
+}
+
 
 /* 
   Add your pseudocode here
 
-  Approach1:
-  First iterate over a string in forward and backward direction.
+ if the input is the same as the reversed input
+      return true
+  else
+      return false 
 
-  Check if all foward and backward character matches, return true.
-
-  if all forward and backward character does not match, return false
-  else return is true, it is a palindrome.
 */
 
 
 
 /*
   Add written explanation of your solution here
+
+  Function should return true if a word is a palindrome and false if not. since JavaScript doesn't 
+  have inbuilt reverse method. 
+  First will do the following.
+  take word and split into a string using the string method. This will put the word in an array
+  take the split array and reverse it using the reverse method . note reverse method only act on array
+  after reversing the array. its time to join the array. use the join method. to join the array
+
+  return the results of the joined in the function . 
 */
 
 
